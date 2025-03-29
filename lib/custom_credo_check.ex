ab9283c1d33f3ad272131fd31df9a06596aa3526
Gabriel Perales <gabriel.perales@cabify.com>
2025-03-29 13:25:35 +0100

    initial commit

---
+++ lib/custom_credo_check.ex
@@ -0,0 +1,18 @@
+defmodule CustomCredoCheck do
+  @moduledoc """
+  Documentation for `CustomCredoCheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CustomCredoCheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/custom_credo_check_test.exs
@@ -0,0 +1,8 @@
+defmodule CustomCredoCheckTest do
+  use ExUnit.Case
+  doctest CustomCredoCheck
+
+  test "greets the world" do
+    assert CustomCredoCheck.hello() == :world
+  end
+end
